deb493982ea3b90251029fafd9f29296d9448089
Sarah Ayu Nanda <nanda.sarahayu@gmail.com>
2022-07-18 00:04:25 +0700

    create footer component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
 import { ThemeProvider } from '@mui/material';
 
 import theme from './themes/theme';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
         <Navbar/>
+        <Footer/>
       </div>
     </ThemeProvider>
   );
