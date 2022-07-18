1bb3fb1751a0b586e55191920a69a6ac78b72f31
adepane <adesbpane@gmail.com>
2022-07-18 08:51:22 +0700

    menu using in this container

--- src/containers/MovieList.js
+++ src/containers/MovieList.js
@@ -1,10 +1,17 @@
+import { Container } from "@mui/material";
 import React from "react"; 
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 const MovieList = () => {
     return(
-       <>
-            this is the movie list
-       </>
+        <>
+            <Navbar/>
+            <Container sx={{minHeight: '100vh', marginTop: '20vh'}}>
+                this is the movie list
+            </Container>
+            <Footer/>
+        </>
     )
 }
 
