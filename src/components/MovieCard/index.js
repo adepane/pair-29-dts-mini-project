763c4c5cd4347833337f0788d320eeb70f1f7c66
adepane <adesbpane@gmail.com>
2022-07-20 17:38:30 +0700

    finishing movie card component

--- src/components/MovieCard/index.js
+++ src/components/MovieCard/index.js
@@ -4,19 +4,38 @@ import { Card, CardMedia } from "@mui/material";
 
 const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/original";
 
-function MovieCard({ id, img, title, mediaType }) {
+function MovieCard({ id, img, title, mediaType, rank, makerank = false}) {
+    const rankImage =
+      rank <= 10 && makerank ? (
+        <div
+          style={{
+            position: "absolute ",
+            background: `url(../../../assets/images/Rank-${rank}.png)`,
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "right",
+            height: "100%",
+            width: "100%",
+            top: "0",
+            left: "0",
+          }}
+        ></div>
+      ) : (
+        ""
+      );
     return (
       <Link
         to={`/${mediaType === "movie" ? "movie" : "tv"}/${id}`}
         sx={{ margin: "10px" }}
       >
-        <Card sx={{ maxWidth: 345, width: "100%" }}>
+        <Card sx={{ maxWidth: 345, width: "100%", position: 'relative' }}>
           <CardMedia
             component="img"
             height="150"
             image={`${BASE_IMAGE_URL}${img}`}
             alt={title}
           />
+          {rankImage}
         </Card>
       </Link>
     );
