2873831990cb34c0464e294494be39819c128b45
adepane <adesbpane@gmail.com>
2022-07-20 18:34:47 +0700

    modify card movie again :D

--- src/components/MovieCard/index.js
+++ src/components/MovieCard/index.js
@@ -1,45 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Card, CardMedia } from "@mui/material";
+import { Card, CardMedia, Stack } from "@mui/material";
+import { height } from "@mui/system";
 
 const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/original";
 
 function MovieCard({ id, img, title, mediaType, rank, makerank = false, portrait = false}) {
     const rankImage =
       rank <= 10 && makerank ? (
-        <div
-          style={{
-            position: "absolute ",
-            background: `url(../../../assets/images/Rank-${rank}.png)`,
-            backgroundSize: "contain",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "right",
-            height: "100%",
-            width: "100%",
-            top: "0",
-            left: "0",
-          }}
-        ></div>
+        <img src={`../../../assets/images/Rank-${rank}.png`} alt={rank} />
       ) : (
         ""
       );
+    const imgPosition = makerank ? "flex-start" : "center";
     const mediaPortrait = portrait ? '345px' : '150px';
     const cardPortrait = portrait ? '180px' : '345px';
+    const topRankHeight = makerank ? "160px" : mediaPortrait;
     return (
       <Link
         to={`/${mediaType === "movie" ? "movie" : "tv"}/${id}`}
         sx={{ margin: "10px" }}
       >
         <Card
-          sx={{ maxWidth: cardPortrait, width: "100%", position: "relative" }}
+          sx={{ maxWidth: cardPortrait, width: "100%", position: "relative", background: "#232323"}}
         >
-          <CardMedia
-            component="img"
-            height={mediaPortrait}
-            image={`${BASE_IMAGE_URL}${img}`}
-            alt={title}
-          />
-          {rankImage}
+          <CardMedia height={topRankHeight}>
+            <Stack
+              direction="row"
+              sx={{ width: "100%", overflow: "auto", height: topRankHeight }}
+              justifyContent={imgPosition}
+            >
+              {rankImage}
+              <img src={BASE_IMAGE_URL + img} alt={title} style={{height: "100%"}} />
+            </Stack>
+          </CardMedia>
         </Card>
       </Link>
     );
